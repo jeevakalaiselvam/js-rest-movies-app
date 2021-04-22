f7bafa0d2f1544ce530122cef32b20e8b08d3ca3
Jeeva Kalaiselvam <jeevak001@outlook.com>
2021-04-23 05:09:01 +0530

    Search Form complete

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
+//Configure and store your REST API endpoints and API here
+//!IMPORTANT Storing API here is not advised, API keys must be encrypted and should not made visible to public. This is just for demonstration
 const APIKEY = "028cb94d290fbb9fdea5031c90628bd2";
 const APIURL =
     "https://api.themoviedb.org/3/discover/movie?api_key=028cb94d290fbb9fdea5031c90628bd2&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate";
 const IMGPATH = "https://image.tmdb.org/t/p/w1280/";
 
 const main = document.querySelector("main");
+const form = document.querySelector();
 
+//Get all popular movies to populate initially
+
+getMovies();
+
+/**
+ * @author  Jeeva Kalaiselvam
+ * @returns @Promise Gets all popular movie data from REST endpoint
+ */
 async function getMovies() {
     const response = await fetch(APIURL);
     const responseData = await response.json();
@@ -31,6 +42,11 @@ async function getMovies() {
     return responseData;
 }
 
+/**
+ * @author Jeeva Kalaiselvam
+ * @param {Number} voteAverage Defines the movie average rating from popularity
+ * @returns {String} A class name to be inserted into the span to highlight different rating by different colors
+ */
 function getClassByRating(voteAverage) {
     if (voteAverage >= 8) {
         return "green";
